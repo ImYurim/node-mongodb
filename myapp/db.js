7a05dda303f517a9cc5c462ce5f1784bed154823
임유림 <ymyu124@naver.com>
2020-08-04 18:13:24 +0900

    join-form

--- myapp/db.js
+++ myapp/db.js
@@ -12,34 +12,8 @@ db.once('open', function  () {
       console.log("open");
 });
 
-//db schema
-// creates DB schema
-// var userSchema = mongoose.Schema({
-//   username: 'string',
-//   age: 'number'
-// });
-
-// // compiels our schema into a model
-// var User = mongoose.model('User', userSchema);
-
-// var user1 = new User({ username: 'gchoi', age: 30 });
-// var user2 = new User({ username: 'jmpark', age: 29 });
- 
-
-// // save user1
-// user1.save(function (err, user1) {
-// if (err) // TODO handle the error
-//     console.log("error");
-// });
-
-// // save user2
-// user2.save(function (err, user2) {
-// if (err) // TODO handle the error
-//     console.log("error");
-// });
 
 
-//mongo
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 
